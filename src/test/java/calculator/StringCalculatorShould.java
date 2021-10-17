bdb7833806ab8726fccd6a6fa21d15acdb5f14ec
shubhjoshi1595 <92647050+shubhjoshi1595@users.noreply.github.com>
2021-10-17 23:12:13 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,10 +1,23 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
+    
+     private StringCalculator stringCalculator;
+    
+     @Before
+    public void initialize() {
+        stringCalculator = new StringCalculator();
+    }
+    
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     void empty_string_should_return_0() {
@@ -17,4 +30,37 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+     @Test
+    public void numbersCommaDelimitedShouldBeSummed() {
+        assertEquals(stringCalculator.add("1,2"), 3);
+        assertEquals(25, stringCalculator.add("10,15"));
+    }
+
+    @Test
+    public void numbersNewlineDelimitedShouldBeSummed() {
+        assertEquals(stringCalculator.add("1\n2"), 3);
+        assertEquals(stringCalculator.add("11\n13"), 24);
+    }
+    
+      @Test
+    public void threeNumbersDelimitedAnywayShouldBeSummed() {
+        assertEquals(stringCalculator.add("1,2,3"), 6);
+        assertEquals(stringCalculator.add("5\n2\n3"), 10);
+    }
+
+    @Test
+    public void negativeInputReturnsException() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Negative input!");
+        stringCalculator.add("-1");
+        stringCalculator.add("-5,10\n-15");
+    }
+
+    @Test
+    public void numbersGreaterThan1000AreIgnored() {
+        assertEquals(stringCalculator.add("5,12,1001"), 17);
+        assertEquals(StringCalculator.add("14124,22\n4,1214"), 26);
+    }
+}
 }
